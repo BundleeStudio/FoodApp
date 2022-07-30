8efbae855a19259ce1590f0f3c30a3f8dd56136f
clement <clementpnc@gmail.com>
2022-07-30 21:03:17 +0200

    feat : add react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,16 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import Home from "./pages/Home"
+import Receipt from "./pages/Receipt"
+
 function App() {
   return (
-    <h1>Test</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/receipt" element={<Receipt />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
