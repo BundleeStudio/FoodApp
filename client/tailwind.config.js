e98960e25bd1166887a1ff5db0d44d5aae4f8a85
clement <clementpnc@gmail.com>
2022-08-11 22:00:21 +0200

    feat : add font

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -7,7 +7,26 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        snow: '#FFFEFE',
+        smoky: '#222222',
         cta: "#519872",
+        greys: {
+          100: '#EBEBEB',
+          200: '#9D9D9D',
+          300: '#E0E0E0',
+          400: '#4E4E4E',
+          500: 'rgba(115,115,115,0.8)'
+        },
+      },
+      fontFamily: {
+        PoppinsBlack: 'PoppinsBlack',
+        PoppinsExtraBold: 'PoppinsExtraBold',
+        PoppinsBold: 'PoppinsBold',
+        PoppinsSemiBold: 'PoppinsSemiBold',
+        PoppinsMedium: 'PoppinsMedium',
+        PoppinsRegular: 'PoppinsRegular',
+        PoppinsLight: 'PoppinsLight',
+        PoppinsExtraLight: 'PoppinsExtraLight',
       }
     },
   },
